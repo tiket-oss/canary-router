4a25266107850c3a3c12e5a06f9c46f2397d5640
Fahri Nurul Hidayat <fahri.hidayat@tiket.com>
2019-08-05 11:59:54 +0700

    Extract as independent test: 'Test supported HTTP methods'

--- handler/index_test.go
+++ handler/index_test.go
@@ -32,81 +32,83 @@ func Test_viaProxy_integration(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	testCases := []struct {
-		name          string
-		argStatusCode int
-		wantBody      []byte
-	}{{
-		name:          "forward to Main",
-		argStatusCode: canaryrouter.StatusCodeMain,
-		wantBody:      []byte(backendMainBody),
-	}, {
-		name:          "forward to Canary",
-		argStatusCode: canaryrouter.StatusCodeCanary,
-		wantBody:      []byte(backendCanaryBody),
-	}}
+	t.Run("Test supported HTTP methods", func(t *testing.T) {
+		testCases := []struct {
+			name          string
+			argStatusCode int
+			wantBody      []byte
+		}{{
+			name:          "forward to Main",
+			argStatusCode: canaryrouter.StatusCodeMain,
+			wantBody:      []byte(backendMainBody),
+		}, {
+			name:          "forward to Canary",
+			argStatusCode: canaryrouter.StatusCodeCanary,
+			wantBody:      []byte(backendCanaryBody),
+		}}
 
-	methods := []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodPatch, http.MethodDelete}
+		methods := []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodPatch, http.MethodDelete}
 
-	for _, tc := range testCases {
-		tc := tc
-		t.Run(fmt.Sprintf("%d %s", tc.argStatusCode, tc.name), func(t *testing.T) {
-			//t.Parallel()
+		for _, tc := range testCases {
+			tc := tc
+			t.Run(fmt.Sprintf("%d %s", tc.argStatusCode, tc.name), func(t *testing.T) {
+				//t.Parallel()
 
-			bodyResults := map[string]sidecar.OriginRequest{}
+				bodyResults := map[string]sidecar.OriginRequest{}
 
-			backendSidecar, backendSidecarURL := setupServer(t, []byte("Static sidecar body"), tc.argStatusCode, func(r *http.Request) {
-				decoder := json.NewDecoder(r.Body)
-				var oriReq sidecar.OriginRequest
-				err := decoder.Decode(&oriReq)
-				if err != nil {
-					t.Fatal(err)
-				}
-
-				bodyResults[oriReq.Method] = oriReq
-			})
-			defer backendSidecar.Close()
-
-			thisRouter := httptest.NewServer(http.HandlerFunc(viaProxy(proxies, &http.Client{}, backendSidecarURL.String())))
-			defer thisRouter.Close()
-
-			originBodyContent := "This is DUMMY body"
-
-			for _, m := range methods {
-				t.Run(m, func(t *testing.T) {
-					//t.Parallel()
-
-					req, err := newRequest(m, thisRouter.URL+"/foo/bar", originBodyContent)
+				backendSidecar, backendSidecarURL := setupServer(t, []byte("Static sidecar body"), tc.argStatusCode, func(r *http.Request) {
+					decoder := json.NewDecoder(r.Body)
+					var oriReq sidecar.OriginRequest
+					err := decoder.Decode(&oriReq)
 					if err != nil {
 						t.Fatal(err)
 					}
 
-					resp, err := thisRouter.Client().Do(req)
-					if err != nil {
-						t.Fatal(err)
-					}
-
-					gotBody, err := ioutil.ReadAll(resp.Body)
-					if err != nil {
-						t.Fatal(err)
-					}
-
-					if string(gotBody) != string(tc.wantBody) {
-						t.Errorf("argStatusCode = %d got = %+v; want = %+v", tc.argStatusCode, gotBody, tc.wantBody)
-						t.Errorf("(STR) argStatusCode = %d got = %+v; want = %+v", tc.argStatusCode, string(gotBody), string(tc.wantBody))
-					}
-
+					bodyResults[oriReq.Method] = oriReq
 				})
-			}
+				defer backendSidecar.Close()
 
-			for _, gotOriReq := range bodyResults {
-				if gotOriReq.Body != originBodyContent {
-					t.Errorf("Got ori body content: %s Want: %s", gotOriReq.Body, originBodyContent)
+				thisRouter := httptest.NewServer(http.HandlerFunc(viaProxy(proxies, &http.Client{}, backendSidecarURL.String())))
+				defer thisRouter.Close()
+
+				originBodyContent := "This is DUMMY body"
+
+				for _, m := range methods {
+					t.Run(m, func(t *testing.T) {
+						//t.Parallel()
+
+						req, err := newRequest(m, thisRouter.URL+"/foo/bar", originBodyContent)
+						if err != nil {
+							t.Fatal(err)
+						}
+
+						resp, err := thisRouter.Client().Do(req)
+						if err != nil {
+							t.Fatal(err)
+						}
+
+						gotBody, err := ioutil.ReadAll(resp.Body)
+						if err != nil {
+							t.Fatal(err)
+						}
+
+						if string(gotBody) != string(tc.wantBody) {
+							t.Errorf("argStatusCode = %d got = %+v; want = %+v", tc.argStatusCode, gotBody, tc.wantBody)
+							t.Errorf("(STR) argStatusCode = %d got = %+v; want = %+v", tc.argStatusCode, string(gotBody), string(tc.wantBody))
+						}
+
+					})
 				}
-			}
 
-		})
-	}
+				for _, gotOriReq := range bodyResults {
+					if gotOriReq.Body != originBodyContent {
+						t.Errorf("Got ori body content: %s Want: %s", gotOriReq.Body, originBodyContent)
+					}
+				}
+
+			})
+		}
+	})
 }
 
 func setupServer(t *testing.T, bodyResp []byte, statusCode int, middleFunc func(r *http.Request)) (*httptest.Server, *url.URL) {
