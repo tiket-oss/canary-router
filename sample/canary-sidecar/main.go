b6e6974c206d41eb28803371a607bcf92240ce81
Hawari Rahman <hawari.rahman17@gmail.com>
2019-08-14 17:40:27 +0700

    Improvement: Use golang's reverse proxy for calling sidecar (#21)
    * Use proxy to call sidecar: maintaining request shape
    
    * Remove unused sidecar HTTP Client
    
    * Proxy transport timeouts are configurable
    
    * New sidecar sample implementation. No need to decode sidecar.OriginRequest.

--- sample/canary-sidecar/main.go
+++ sample/canary-sidecar/main.go
@@ -1,35 +1,32 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
 	canaryrouter "github.com/tiket-libre/canary-router"
-	"github.com/tiket-libre/canary-router/sidecar"
 )
 
 func main() {
 
-	http.HandleFunc("/sidecar", func(w http.ResponseWriter, req *http.Request) {
-		decoder := json.NewDecoder(req.Body)
-		var oriReq sidecar.OriginRequest
-		err := decoder.Decode(&oriReq)
+	http.HandleFunc("/sidecar/", func(w http.ResponseWriter, req *http.Request) {
+		bodyBytes, err := ioutil.ReadAll(req.Body)
 		if err != nil {
-			log.Printf("Failed to decode sidecar.OriginRequest json: +%v", err)
+			log.Printf("Failed to read req.Body: +%v", err)
 
 			w.WriteHeader(canaryrouter.StatusCodeMain)
 			return
 		}
 
-		log.Printf("Origin http req: %+v", oriReq)
+		log.Printf("Origin http req: %+v", req)
 
-		if oriReq.Body == "type=1" {
+		if string(bodyBytes) == "type=1" {
 			w.WriteHeader(canaryrouter.StatusCodeMain)
 			return
-		} else if oriReq.Body == "type=2" {
+		} else if string(bodyBytes) == "type=2" {
 			w.WriteHeader(canaryrouter.StatusCodeCanary)
 			return
 		}
