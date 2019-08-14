4d1a769a9ceb66f5029c09dfc3fe704b9fc3b938
Fahri Nurul Hidayat <fahri.hidayat@tiket.com>
2019-08-14 17:28:29 +0700

    Remove unused function

--- server/server.go
+++ server/server.go
@@ -255,13 +255,3 @@ func recordMetricTarget(ctx context.Context, target string) {
 
 	instrumentation.RecordLatency(ctx)
 }
-
-func cloneHeader(h http.Header) http.Header {
-	h2 := make(http.Header, len(h))
-	for k, vv := range h {
-		vv2 := make([]string, len(vv))
-		copy(vv2, vv)
-		h2[k] = vv2
-	}
-	return h2
-}
